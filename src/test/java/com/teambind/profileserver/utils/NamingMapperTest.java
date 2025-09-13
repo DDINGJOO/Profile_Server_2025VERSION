d8e375b0d602417560764b82789e8f35898b60fe
ddingjoo <ddingsha9@teambind.co.kr>
2025-09-13 18:26:31 +0900

    [Test] NamingMapperTest Interface
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

---
+++ src/test/java/com/teambind/profileserver/utils/NamingMapperTest.java
@@ -0,0 +1,21 @@
+package com.teambind.profileserver.utils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+class NamingMapperTest {
+    private NamingMapper namingMapper;
+
+
+    @Test
+    @DisplayName("convertGenreNameTable, 테스트 ")
+    void convertGenreNameTable() {
+    }
+
+    @Test
+    @DisplayName("convertInstrumentNameTable, 테스트 ")
+    void convertInstrumentNameTable() {
+    }
+
+}

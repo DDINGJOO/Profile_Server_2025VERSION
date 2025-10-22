de33e6343c8829a274cdf26fc6d097b2d9867a4e
ddingjoo <ddingsha9@teambind.co.kr>
2025-10-22 17:07:49 +0900

    [FEAT] UserInfo 엔티티에 UserGenreKey 및 UserInstrumentKey 설정 추가
    src/main/java/com/teambind/profileserver/entity/UserInfo.java: addGenre와 addInstrument 메서드에서 UserGenreKey와 UserInstrumentKey 설정 로직 추가
    
    참고: 사용자와 장르/악기 간 연관 관계 ID 관리 강화를 위해 수정.
    
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

--- src/main/java/com/teambind/profileserver/entity/UserInfo.java
+++ src/main/java/com/teambind/profileserver/entity/UserInfo.java
@@ -3,6 +3,8 @@ package com.teambind.profileserver.entity;
 
 import com.teambind.profileserver.entity.attribute.UserGenres;
 import com.teambind.profileserver.entity.attribute.UserInstruments;
+import com.teambind.profileserver.entity.attribute.key.UserGenreKey;
+import com.teambind.profileserver.entity.attribute.key.UserInstrumentKey;
 import com.teambind.profileserver.entity.attribute.nameTable.GenreNameTable;
 import com.teambind.profileserver.entity.attribute.nameTable.InstrumentNameTable;
 import jakarta.persistence.*;
@@ -69,31 +71,31 @@ public class UserInfo {
 	public void addInstrument(InstrumentNameTable instrument) {
 		if (instrument == null) return;
 		if (this.userInstruments == null) this.userInstruments = new ArrayList<>();
-		
+
 		boolean exists = this.userInstruments.stream()
 				.anyMatch(link -> link.getAttribute() != null
 						&& link.getAttribute().getInstrumentId() == instrument.getInstrumentId());
 		if (exists) return;
-		
+
 		UserInstruments link = new UserInstruments();
 		link.setUserInfo(this);
 		link.setAttribute(instrument);
-		// 필요시 link.setId(new UserInstrumentKey(this.userId, instrument.getInstrumentId()));
+		link.setId(new UserInstrumentKey(this.userId, instrument.getInstrumentId()));
 		this.userInstruments.add(link);
 	}
 	public void addGenre(GenreNameTable genre) {
 		if (genre == null) return;
 		if (this.userGenres == null) this.userGenres = new ArrayList<>();
-		
+
 		boolean exists = this.userGenres.stream()
 				.anyMatch(link -> link.getAttribute() != null
 						&& link.getAttribute().getGenreId() == genre.getGenreId());
 		if (exists) return;
-		
+
 		UserGenres link = new UserGenres();
 		link.setUserInfo(this);
 		link.setAttribute(genre);
-		// 필요시 link.setId(new UserGenreKey(this.userId, genre.getGenreId()));
+		link.setId(new UserGenreKey(this.userId, genre.getGenreId()));
 		this.userGenres.add(link);
 	}
 	

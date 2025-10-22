2c12b776e84be1f04ffa4420352b8c94a5a3775c
ddingjoo <ddingsha9@teambind.co.kr>
2025-10-22 11:25:46 +0900

    [FEAT] 테스트 환경 설정 및 초기 데이터 구성 추가
    src/test/resources/application-test.yaml: 테스트 전용 Spring 설정 추가 (H2 DB, Kafka/Redis 비활성화)
    src/test/resources/data.sql: 테스트용 기본 데이터(Genre, Instrument, Location) 구성
    src/test/java/com/teambind/profileserver/config/TestConfig.java: KafkaTemplate Mock 구현으로 외부 의존성 제거
    
    참고: 테스트 환경 분리 및 DB 초기화 데이터 제공.
    
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

---
+++ src/test/resources/data.sql
@@ -0,0 +1,20 @@
+-- 테스트용 Genre 데이터
+INSERT INTO genre_name (genre_id, genre_name) VALUES (1, 'Rock');
+INSERT INTO genre_name (genre_id, genre_name) VALUES (2, 'Jazz');
+INSERT INTO genre_name (genre_id, genre_name) VALUES (3, 'Classical');
+INSERT INTO genre_name (genre_id, genre_name) VALUES (4, 'Pop');
+INSERT INTO genre_name (genre_id, genre_name) VALUES (5, 'Hip-Hop');
+
+-- 테스트용 Instrument 데이터
+INSERT INTO instrument_name (instrument_id, instrument_name) VALUES (1, 'Guitar');
+INSERT INTO instrument_name (instrument_id, instrument_name) VALUES (2, 'Piano');
+INSERT INTO instrument_name (instrument_id, instrument_name) VALUES (3, 'Drum');
+INSERT INTO instrument_name (instrument_id, instrument_name) VALUES (4, 'Bass');
+INSERT INTO instrument_name (instrument_id, instrument_name) VALUES (5, 'Violin');
+
+-- 테스트용 Location 데이터
+INSERT INTO location_names (city_id, city_name) VALUES ('1', '서울');
+INSERT INTO location_names (city_id, city_name) VALUES ('2', '부산');
+INSERT INTO location_names (city_id, city_name) VALUES ('3', '인천');
+INSERT INTO location_names (city_id, city_name) VALUES ('4', '대구');
+INSERT INTO location_names (city_id, city_name) VALUES ('5', '광주');
